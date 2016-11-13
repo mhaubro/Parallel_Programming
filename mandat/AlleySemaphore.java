e63636a536fb3a1368e7813964f2032584e2663f
Mathias Gammelmark <mathiasgam@hotmail.com>
2016-11-13 15:25:42 +0100

    added some comments

--- mandat/AlleySemaphore.java
+++ mandat/AlleySemaphore.java
@@ -1,36 +1,53 @@
 package mandat;
 
+/**
+ * AlleySemaphore is a implementation of Alley, using semaphores for the
+ * synchronization. Extends the Alley object.
+ * 
+ * @author Martin and Mathias
+ *
+ */
 public class AlleySemaphore extends Alley {
 
+	/** Constructs a AlleySemaphore object. */
 	public AlleySemaphore() {
 		super();
 	}
 
-	private int count = 0; // how many cars in the alley.
+	/** how many cars is in the alley */
+	private int count = 0;
+
+	/** how many cars is waiting to enter the alley. */
 	private int waiting = 0;
 
+	// both semaphores is a way of entering the same critical region. witch
+	// results in a kind of wait- or method-state.
+	/**
+	 * semaphore used for standard entering of the critical region in method
+	 * state
+	 */
 	private Semaphore method_semaphore = new Semaphore(1);
-	private Semaphore is_empty = new Semaphore(0);
+	/** semaphore used to enter the critical region in wait-state */
+	private Semaphore wait = new Semaphore(0);
 
 	/**
+	 * Enter will block until the car n has succesfuly entered the alley.
 	 * 
-	 * @param n,
-	 *            is the identity of the car, who is entering. n is used to
-	 *            determine the direction of the car.
+	 * @param n
+	 *            n is the identity of the car, who is entering.
 	 * @throws InterruptedException,
-	 *             since the method is running on semaphores, who throws the
-	 *             exception
+	 *             can be thrown if the semaphores is interrupted.
 	 */
 	public void enter(int n) throws InterruptedException {
-		/*
-		 * the car will repeatedly check if it can enter the alley, and if not,
-		 * it will wait till it can
-		 */
+		// the car will repeatedly check if it can enter the alley, and if not,
+		// it will wait until it will get notified.
 		while (true) {
+			// enters critical region when the critical region is released to
+			// method_semaphore.
 			method_semaphore.P();
 
-			// checks if the car can enter the alley, by breaking the
-			// waiting-loop
+			// checks if the car can enter the alley. if this is the case the
+			// loop will be broken.
 			if (count < 1) {
 				dir = getDir(n);
 				break; // enter the alley
@@ -38,25 +55,21 @@ public class AlleySemaphore extends Alley {
 				break; // enter the alley
 			}
 
-			/*
-			 * increment the waiting counter so others know that there's a car
-			 * waiting, then leave the critical region and try to enter on the
-			 * "is_empty" semaphore
-			 */
+			// increment the waiting counter so others know that there's a car
+			// waiting, then leave the critical region and try to enter on the
+			// "wait" semaphore.
 			waiting++;
 			method_semaphore.V();
-			is_empty.P();
-
-			/*
-			 * decrement the waiting counter, and check whether or not there are
-			 * more cars, who is waiting. If this is the case the next will be
-			 * notified by leaving the critical region with is_empty. else the
-			 * region will be left with the method semaphore, so everyone else
-			 * can continue.
-			 */
+			wait.P();
 			waiting--;
+
+			// decrement the waiting counter, and check whether or not there are
+			// more cars, who is waiting. If this is the case the next will be
+			// notified by leaving the critical region with "wait.V()". else the
+			// region will be left with the method semaphore, so everyone else
+			// can continue.
 			if (waiting > 0) {
-				is_empty.V();
+				wait.V();
 			} else {
 				method_semaphore.V();
 			}
@@ -68,12 +81,18 @@ public class AlleySemaphore extends Alley {
 		return;
 	}
 
+	/**
+	 * Leaves the alley as the car with id n. and will then notify the next car
+	 * who waits, that it can enter.
+	 * @param n n is the id of the car
+	 * @throws InterruptedException, if the semaphore is interupted.
+	 */
 	public void leave(int n) throws InterruptedException {
 		// display.println("m leave");
 		method_semaphore.P(); // Enter critical region
 		count--;
 		if (count < 1 && waiting > 0) {
-			is_empty.V();
+			wait.V();
 		} else {
 			method_semaphore.V(); // leave critical region
 		}
